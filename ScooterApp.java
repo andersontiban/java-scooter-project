f58637fca4991ce7e09ac83de7b7acbd02608f8b
Anderson Tiban <andytiban12@gmail.com>
2023-02-28 20:28:49 -0500

    added

--- ScooterApp.java
+++ ScooterApp.java
@@ -3,15 +3,20 @@ import java.util.ArrayList;
 
 public class ScooterApp{
    //instance fields
-   private Hashtable<String, ArrayList<String>> locations;   
+   private Hashtable<String, ArrayList<String>> locations;
+   //declaring list for locations hashtable
+   ArrayList<String> loc1 = new ArrayList<String>();
+   ArrayList<String> loc2 = new ArrayList<String>(); 
+   ArrayList<String> loc3 = new ArrayList<String>();     
    
    //constructor
    ScooterApp(){
+      //initilaize hashtable
       this.locations = new Hashtable<String, ArrayList<String>>();
-      ArrayList<String> loc1 = new ArrayList<String>();
-      ArrayList<String> loc2 = new ArrayList<String>();
+      //add list as value in hashtable
       this.locations.put("loc1", loc1);
       this.locations.put("loc2", loc2);
+      this.locations.put("loc3", loc3);
    }
 
 
@@ -19,6 +24,12 @@ public class ScooterApp{
    
    public static void main(String[] args){
       ScooterApp app = new ScooterApp();
+      
+    
+      app.loc1.add("scoot");
+      
+      app.loc3.add("scoot2");
+      
       System.out.println(app.locations);
    }
    
